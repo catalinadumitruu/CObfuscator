9567ee10e3d00f648ea51bdeb23fc22be2739dd2
Dumitru Catalina <catalina.dumitru1998@gmail.com>
2022-01-06 20:35:19 +0200

    Finish homework
    Finish homework

---
+++ CObfuscator/src/addition-1903335959.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#define _goto int
+#define r return
+#define __goto float
+#define ___goto char*
+#define _999 0
+#define _888_ 1
+#define _777 2
+#define _99_ 3
+#define _0_0 4
+#define _555 5
+#define __9 6
+#define _090__ 7
+#define ____9_1 8
+#define n9__9 9
+#define ___ main()
+#define _ sum
+#define __________ getchar();
+_goto ___ {  _goto number1, number2, sum;  printf("Enter two integers: "); scanf("%d %d", &number1, &number2);  _ = number1 + number2;   printf("%d + %d = %d", number1, number2, sum);   __________   __________     r 0;}
