d6f301627513a776320f102720950adca22c0aa6
Dumitru Catalina <catalina.dumitru1998@gmail.com>
2022-01-08 19:06:26 +0200

    Adapt code
    Adapt code

---
+++ CObfuscator/src/example1-1609543580.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#define _goto int
+#define r return
+#define _____ char
+#define ___goto void
+#define _999 0;}
+#define _888_ 1,
+#define _777 2,
+#define _99_ 3
+#define _0_0 4
+#define _555 5,
+#define __9 100,
+#define _090__ 10,
+#define ____9_1 20,
+#define n9__9 2000
+#define ___ main(){
+#define _ sum
+#define __________ getchar();
+#define __________________ max
+#define if_then name);
+#define __ i
+#define exit_ largest
+#define goup return
+#define ____________________________ cost
+#define a for
+_goto ___ _____ name[20]; printf("Your name: "); scanf("%s", if_then printf("Hello, %s!", if_then __________ r _999
---
+++ CObfuscator/src/example2474648686.c
@@ -0,0 +1,27 @@
+
+#include <stdio.h>
+#define _goto int
+#define r return
+#define _____ char
+#define ___goto void
+#define _999 0;}
+#define _888_ 1,
+#define _777 2,
+#define _99_ 3
+#define _0_0 4
+#define _555 5,
+#define __9 100,
+#define _090__ 10,
+#define ____9_1 20,
+#define n9__9 2000
+#define ___ main(){
+#define _ sum
+#define __________ getch();
+#define __________________ max
+#define if_then name);
+#define __ i
+#define exit_ largest
+#define goup return
+#define ____________________________ cost
+#define a for
+_goto largest(int arr[], _goto n){ _goto i;  _goto __________________ = arr[0];   a (i = 1; __ < n; i++)  if (arr[i] > max)max = arr[i]; r max;}int ___ _goto arr[] = {10, 324, 45, 90, 9808}; _goto n = sizeof(arr)/sizeof(arr[0]); printf("Largest in given array is %d", largest(arr, n)); r _999
